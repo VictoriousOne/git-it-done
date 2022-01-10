b5fe9c5b3374196681d78fba395d7b1c609c3ed3
Kerry Long <victory173451@gmail.com>
2022-01-10 07:01:23 -0500

    language query parameter

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,6 +4,24 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    });
+  };
+
 var displayRepos = function (repos, searchTerm) {
 
 
@@ -89,4 +107,17 @@ var formSubmitHandler = function (event) {
     }
 };
 
+var buttonClickHandler = function(event) {
+
+    language = event.target.getAttributs("data-language");
+
+    if (language) {
+        getFeaturedRepos(language);
+      
+        // clear old content
+        repoContainerEl.textContent = "";
+      }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
